ee9af2e36bb7472d890e013bc0ed6156359d07b2
DESKTOP-K9R1U8L\Phua Jun Jie <e0127332@u.nus.edu>
2017-09-14 11:21:06 +0800

    Added JUNIT TEST

--- test/java/seedu/addressbook/common/UtilsTest.java
+++ test/java/seedu/addressbook/common/UtilsTest.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class UtilsTest {
-
+// Junit test for isAnyNull
     @Test
     public void isAnyNull() throws Exception {
         //empty list
